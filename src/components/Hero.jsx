92990838322bed9863d1b0a99f1a5a25cf17c30e
Mani Chaturvedi <manichaturvedi17@gmail.com>
2025-03-15 19:25:27 +0530

    navbar and about done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {HERO_CONTENT} from"../constants"
+import profilePic from "../assets/KevinRushProfile.png"
+
+const Hero = () => {
+  return <div className='border-b border-neutal-900 pb-4 lg:mb-35'>
+    <div className='flex flex-wrap'>
+        <div className='w-full lg:w-1/2'>
+            <div className='flex flex-col items-center lg:items-start'>
+                <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>
+                    Mani Chaturvedi
+                </h1>
+                <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>
+                    Full Stack Developer
+                </span>
+                <p className='my-2 max-w-xl py-6 font-light tracking-tigher'>{HERO_CONTENT}</p>
+            </div>
+        </div>
+        <div className='w-full lg:w-1/2'>
+            <div className='flex justify-center'>
+                <img className='rounded-full w-64 h-64' src={profilePic} alt='Kevin Rush'/>
+            </div>
+        </div>    
+    </div>
+  </div>
+}
+
+export default Hero
